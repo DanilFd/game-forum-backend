b7e992232682fd052305f7500754760e55ee0716
DanilFD <happyaggresor@mail.ru>
2022-05-06 21:03:35 +0300

    add validation in blog model

--- blogs/models.py
+++ blogs/models.py
@@ -1,4 +1,4 @@
-from django.core.validators import MaxValueValidator
+from django.core.validators import MaxValueValidator, MinLengthValidator
 from django.db import models
 
 # Create your models here.
@@ -8,9 +8,11 @@ from users.models import CustomUser
 class Blog(models.Model):
     class Meta:
         ordering = ['creation_date']
+        verbose_name = "Блог"
+        verbose_name_plural = "Блоги"
 
     img = models.ImageField(verbose_name="Изображение")
-    title = models.CharField(verbose_name="Название", max_length=50, unique=True)
+    title = models.CharField(verbose_name="Название", max_length=100, validators=[MinLengthValidator(5)], unique=True)
     creation_date = models.DateField(verbose_name="Дата создания", auto_now_add=True)
     creator = models.ForeignKey(CustomUser, verbose_name="Создатель", on_delete=models.CASCADE, blank=True)
     rating = models.IntegerField(default=0, verbose_name="Рейтинг")
